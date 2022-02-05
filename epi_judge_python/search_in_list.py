c5ac9e049b9c06a7b4266fad1c3e349862ec52be
cwaltz <anighadge@yahoo.com>
2022-02-05 17:53:29 +0530

    solved 7.00

--- epi_judge_python/search_in_list.py
+++ epi_judge_python/search_in_list.py
@@ -3,8 +3,10 @@ from test_framework import generic_test
 
 
 def search_list(L: ListNode, key: int) -> ListNode:
-    # TODO - you fill in here.
-    return ListNode()
+    while L and L.data != key:
+        L = L.next
+    # If key was not present in the list, L will have become null.
+    return L
 
 
 def search_list_wrapper(L, key):
