7268c10f876ef0928ff0eb0df3dcabc095f68c4c
cwaltz <anighadge@yahoo.com>
2022-02-19 12:16:29 +0530

    solved 8.01

--- epi_judge_python/stack_with_max.py
+++ epi_judge_python/stack_with_max.py
@@ -1,23 +1,92 @@
+import collections
+from typing import List
+
 from test_framework import generic_test
 from test_framework.test_failure import TestFailure
 
 
 class Stack:
+    """
+    The worst-case additional space complexity is O(n), which occurs when each key pushed is greater
+    than all keys in the primary stack. However, when the number of distinct keys is small, or the
+    maximum changes infrequently, the additional space complexity is less, O(1) in the best-case. The
+    time complexity for each specified method is still O(1).
+    """
+
+    class MaxWithCount:
+        def __init__(self, max: int, count: int) -> None:
+            self.max = max
+            self.count = count
+
+    def __init__(self) -> None:
+        self._elements: List[int] = []
+        self._cached_max_with_count: List[Stack.MaxWithCount] = []
+
     def empty(self) -> bool:
-        # TODO - you fill in here.
-        return True
+        return len(self._elements) == 0
 
     def max(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError('max(): empty stack')
+        return self._cached_max_with_count[-1].max
 
     def pop(self) -> int:
-        # TODO - you fill in here.
-        return 0
+        if self.empty():
+            raise IndexError('pop(): empty stack')
+        popped = self._elements.pop()
+        current_max = self._cached_max_with_count[-1].max
+        if popped == current_max:
+            self._cached_max_with_count[-1].count -= 1
+            if self._cached_max_with_count[-1].count == 0:
+                self._cached_max_with_count.pop()
+        return popped
 
     def push(self, x: int) -> None:
-        # TODO - you fill in here.
-        return
+        self._elements.append(x)
+        if len(self._cached_max_with_count) == 0:
+            self._cached_max_with_count.append(self.MaxWithCount(x, 1))
+        else:
+            current_max = self._cached_max_with_count[-1].max
+            if x > current_max:
+                self._cached_max_with_count.append(self.MaxWithCount(x, 1))
+            elif x == current_max:
+                self._cached_max_with_count[-1].count += 1
+
+
+class StackV1:
+    """
+    Each of the specified methods has time complexity O(1).
+    The additional space complexity is O(n), regardless of the stored keys.
+    """
+
+    ElementWithCachedMax = collections.namedtuple('ElementWithCachedMax',
+                                                  ('element', 'max'))
+
+    def __init__(self) -> None:
+
+        self._elements: List[StackV1.ElementWithCachedMax] = []
+
+    def empty(self) -> bool:
+
+        return len(self._elements) == 0
+
+    def max(self) -> int:
+
+        if self.empty():
+            raise IndexError('max(): stack is empty')
+        return self._elements[-1].max
+
+    def pop(self) -> int:
+
+        if self.empty():
+            raise IndexError('pop(): stack is empty')
+        return self._elements.pop().element
+
+    def push(self, x: int) -> None:
+
+        self._elements.append(
+            self.ElementWithCachedMax(
+                x, x if self.empty() else max(x, self.max())))
 
 
 def stack_tester(ops):
