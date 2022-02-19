efb4fd5eb45cd3cd3fda1ac0ed3363096d943d39
cwaltz <anighadge@yahoo.com>
2022-02-19 23:55:31 +0530

    solved 8.06

--- epi_judge_python/evaluate_rpn.py
+++ epi_judge_python/evaluate_rpn.py
@@ -1,9 +1,26 @@
+from typing import List
+
 from test_framework import generic_test
 
 
 def evaluate(expression: str) -> int:
-    # TODO - you fill in here.
-    return 0
+
+    intermediate_results: List[int] = []
+    DELIMITER = ','
+    OPERATORS = {
+        '+': lambda y, x: x + y,
+        '-': lambda y, x: x - y,
+        '*': lambda y, x: x * y,
+        '/': lambda y, x: x // y  # integer division
+    }
+
+    for token in expression.split(DELIMITER):
+        if token in OPERATORS:
+            intermediate_results.append(OPERATORS[token](
+                intermediate_results.pop(), intermediate_results.pop()))
+        else:  # token is a number
+            intermediate_results.append(int(token))
+    return intermediate_results[-1]
 
 
 if __name__ == '__main__':
