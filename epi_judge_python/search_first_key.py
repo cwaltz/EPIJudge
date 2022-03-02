e52a81cb9148bd1bb8649fd81a95d8da73e681d0
cwaltz <anighadge@yahoo.com>
2022-03-02 19:45:30 +0530

    completed 11. Searching boot camp, solved 11.01

--- epi_judge_python/search_first_key.py
+++ epi_judge_python/search_first_key.py
@@ -4,8 +4,27 @@ from test_framework import generic_test
 
 
 def search_first_of_k(A: List[int], k: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    Time complexity  = O(log n)
+    Space complexity = O(1)
+
+    Test PASSED (314/314) [  12 us]
+    Average running time:    2 us
+    Median running time:     2 us
+    """
+
+    left, right, result = 0, len(A) - 1, -1
+    # A[left:right + 1] is the candidate set.
+    while left <= right:
+        mid = left + (right - left) // 2
+        if A[mid] < k:
+            left = mid + 1
+        elif A[mid] == k:
+            result = mid
+            right = mid - 1  # Nothing to the right of mid can be solution.
+        else:  # k < A[mid]
+            right = mid - 1
+    return result
 
 
 if __name__ == '__main__':
