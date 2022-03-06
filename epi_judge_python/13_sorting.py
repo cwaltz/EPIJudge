f17b56909f2064048ec3c2bb39832d9657f1a480
cwaltz <anighadge@yahoo.com>
2022-03-06 16:14:55 +0530

    solved 13.01

---
+++ epi_judge_python/13_sorting.py
@@ -0,0 +1,49 @@
+from typing import List
+
+
+class Student:
+    def __init__(self, name: str, grade_point_average: float) -> None:
+        self.name = name
+        self.grade_point_average = grade_point_average
+
+    def __lt__(self, other: 'Student') -> bool:
+        return self.name < other.name
+
+
+def print_students(students: List[Student]) -> None:
+    for s in students:
+        print(s.name, s.grade_point_average)
+
+
+students = [
+    Student('A', 4.0),
+    Student('C', 3.0),
+    Student('B', 2.0),
+    Student('D', 3.2),
+]
+print('\nstudents:')
+print_students(students)
+
+# Sort according to __lt__ defined in Student. students remained unchanged.
+students_sort_by_name = sorted(students)
+print('\nstudents sorted by name:')
+print_students(students_sort_by_name)
+
+# Sort students in-place by grade_point_average.
+students.sort(key=lambda student: student.grade_point_average)
+print('\nstudents sorted by gpa:')
+print_students(students)
+
+
+def print_list(nums: List[int]) -> None:
+    for n in nums:
+        print(n)
+
+
+print('\n\nKnow your sorting libraries')
+a = [1, 2, 4, 3, 5, 0, 11, 21, 100]
+print('\na =', a)
+a.sort(key=lambda x: str(x), reverse=True)
+print('\na.sort(key=lambda x: str(x), reverse=True) =', a)
+b = sorted(a, key=lambda x: str(x))
+print('\nsorted(a, key=lambda x: str(x)) =', b)
--- epi_judge_python/intersect_sorted_arrays.py
+++ epi_judge_python/intersect_sorted_arrays.py
@@ -1,11 +1,62 @@
+import bisect
 from typing import List
 
 from test_framework import generic_test
 
 
 def intersect_two_sorted_arrays(A: List[int], B: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    Time complexity = O(m + n) where m = len(A) and n = len(B)
+    Space complexity = O(1)
+
+    Test PASSED (202/202) [  15 ms]
+    Average running time:   86 us
+    Median running time:     6 us
+    """
+    i, j, result = 0, 0, []
+    while i < len(A) and j < len(B):
+        if A[i] == B[j]:
+            if i == 0 or A[i] != A[i - 1]:
+                result.append(A[i])
+            i, j = i + 1, j + 1
+        elif A[i] < B[j]:
+            i += 1
+        else:  # B[j] < A[i]
+            j += 1
+    return result
+
+
+def intersect_two_sorted_arrays_v1(A: List[int], B: List[int]) -> List[int]:
+    """
+    Time complexity = O(m log n) where m = length of smaller array and n = length of larger array
+    Space complexity = O(1)
+
+    Test PASSED (202/202) [  14 ms]
+    Average running time:   83 us
+    Median running time:     6 us
+    """
+
+    def is_present(nums: List[int], a: int) -> bool:
+        i = bisect.bisect_left(nums, a)
+        return i < len(nums) and a == nums[i]
+
+    if len(A) < len(B):
+        return [a for i, a in enumerate(A) if (i == 0 or a != A[i - 1]) and is_present(B, a)]
+    else:  # len(B) < len(A)
+        return [b for j, b in enumerate(B) if (j == 0 or b != B[j - 1]) and is_present(A, b)]
+
+
+def intersect_two_sorted_arrays_v0(A: List[int], B: List[int]) -> List[int]:
+    """
+    Time complexity = O(m * n) where m = len(A) and n = len(B)
+    Space complexity = O(1)
+
+    Test PASSED (202/202) [   4  s]
+    Average running time:   23 ms
+    Median running time:     4 us
+    """
+
+    return [a for i, a in enumerate(A) if (i == 0 or a != A[i - 1]) and a in B]
 
 
 if __name__ == '__main__':
