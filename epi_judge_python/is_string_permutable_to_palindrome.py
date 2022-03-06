e383066e0f2e1b77533f1cc6d26a636286b493bf
cwaltz <anighadge@yahoo.com>
2022-03-06 14:37:40 +0530

    solved 12.01, completed 13. Sorting boot camp

--- epi_judge_python/is_string_permutable_to_palindrome.py
+++ epi_judge_python/is_string_permutable_to_palindrome.py
@@ -2,8 +2,19 @@ from test_framework import generic_test
 
 
 def can_form_palindrome(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    """
+    Test PASSED (305/305) [  29 ms]
+    Average running time:   97 us
+    Median running time:     2 us
+    """
+
+    odd_count = set()
+    for c in s:
+        if c in odd_count:
+            odd_count.remove(c)
+        else:
+            odd_count.add(c)
+    return len(odd_count) < 2
 
 
 if __name__ == '__main__':
--- epi_judge_python/nearest_repeated_entries.py
+++ epi_judge_python/nearest_repeated_entries.py
@@ -1,3 +1,4 @@
+import typing
 from typing import Dict, List
 
 from test_framework import generic_test
@@ -6,13 +7,12 @@ from test_framework import generic_test
 def find_nearest_repetition(paragraph: List[str]) -> int:
 
     last_index_of_word: Dict[str, int] = {}
-    minimum_distance: int = -1
+    minimum_distance = float('inf')
     for index, word in enumerate(paragraph):
         if word in last_index_of_word:
-            # minimum_distance = min(minimum_distance, index - last_index_of_word[word]) if minimum_distance != -1 else -1
-            minimum_distance = index - last_index_of_word[word] if minimum_distance == -1 else min(minimum_distance, index - last_index_of_word[word])
+            minimum_distance = min(minimum_distance, index - last_index_of_word[word])
         last_index_of_word[word] = index
-    return minimum_distance
+    return typing.cast(int, minimum_distance) if minimum_distance != float('inf') else -1
 
 
 if __name__ == '__main__':
