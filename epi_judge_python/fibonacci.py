e9c476f0bcfc27df79bf5770f28b64e04cf8fe75
cwaltz <anighadge@yahoo.com>
2022-04-19 08:20:09 +0530

    partially completed 16. Dynamic Programming boot camp

--- epi_judge_python/fibonacci.py
+++ epi_judge_python/fibonacci.py
@@ -1,9 +1,16 @@
+from typing import List
+
 from test_framework import generic_test
 
 
 def fibonacci(n: int) -> int:
-    # TODO - you fill in here.
-    return -1
+    if n < 2:
+        return n
+    fib: List[int] = [0] * (n + 1)
+    fib[0], fib[1] = 0, 1
+    for i in range(2, n + 1):
+        fib[i] = fib[i - 1] + fib[i - 2]
+    return fib[n]
 
 
 if __name__ == '__main__':
