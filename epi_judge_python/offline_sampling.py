fc1de7ec2986ab94c66e16ecdfb8e96f3fcb3465
marcostar <sagarghadge24@gmail.com>
2022-07-21 01:04:29 +0530

    solved 5.12, 5.18

--- epi_judge_python/offline_sampling.py
+++ epi_judge_python/offline_sampling.py
@@ -12,25 +12,38 @@ from test_framework.test_utils import enable_executor_hook
 def random_sampling(k: int, A: List[int]) -> None:
     """
     #5.12
-    The algorithm clearly runs in additional O(1) space. The time complexity is O(min(k, n - k)) to select the elements.
+
+    Time complexity = O(min(k, n - k)) to select the elements.
+    Space complexity = O(1)
+
     The algorithm makes min(k, n - k) calls to the random number generator. When k is bigger than n / 2, we optimize by
     computing a subset of n - k elements to remove from the set. For example, when k = n - 1, this replaces n - 1 calls
     to the random number generator with a single call.
+
+    Test PASSED (8/8) [ 243 ms]
+    Average running time:  162 ms
+    Median running time:   152 ms
     """
-    if k <= len(A) // 2:
+    n = len(A)
+    if k <= n // 2:
         for i in range(k):
             # Generate a random index r in [i, len(A) - 1]. A[r] is included in the subset.
-            r = random.randint(i, len(A) - 1)
+            r = random.randint(i, n - 1)
             A[i], A[r] = A[r], A[i]
     else:
-        for i in range(len(A) - k):
-            # Generate a random index r in [0, -1 - i]. A[r] is excluded from the subset.
-            r = random.randint(0, len(A) - 1 - i)
-            A[-1 - i], A[r] = A[r], A[-1 - i]
+        for i in range(n - 1, k - 1, -1):
+            # Generate a random index r in [0, i]. A[r] is excluded from the subset.
+            r = random.randint(0, i)
+            A[i], A[r] = A[r], A[i]
 
 
 # Pythonic solution
 def random_sampling_pythonic(k, A):
+    """
+    Test PASSED (8/8) [ 268 ms]
+    Average running time:  269 ms
+    Median running time:   268 ms
+    """
     A[:] = random.sample(A, k)
 
 
--- epi_judge_python/spiral_ordering.py
+++ epi_judge_python/spiral_ordering.py
@@ -3,10 +3,104 @@ from typing import List
 from test_framework import generic_test
 
 
-def matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+def matrix_in_spiral_order(matrix: List[List[int]]) -> List[int]:
     """
     #5.18
+
+    Time complexity = O(r * c) where r and c are number of rows and columns.
+    Space complexity = O(1)
+
+    This solution works for non-square as well as square matrices. Easy to understand and implement.
+
+    Test PASSED (51/51) [ 237 us]
+    Average running time:   87 us
+    Median running time:    67 us
+    """
+    last_row = len(matrix) - 1
+    if last_row == -1:
+        return []
+    if last_row == 0:
+        return matrix[0]
+    last_col = len(matrix[0]) - 1
+    if last_col == 0:
+        return [matrix[row][0] for row in range(last_row + 1)]
+
+    result = []
+    first_row, first_col = 0, 0
+
+    while True:
+        if last_col < first_col:
+            break
+        for col in range(first_col, last_col + 1):
+            result.append(matrix[first_row][col])
+        first_row += 1
+
+        if last_row < first_row:
+            break
+        for row in range(first_row, last_row + 1):
+            result.append(matrix[row][last_col])
+        last_col -= 1
+
+        if last_col < first_col:
+            break
+        for col in range(last_col, first_col - 1, -1):
+            result.append(matrix[last_row][col])
+        last_row -= 1
+
+        if last_row < first_row:
+            break
+        for row in range(last_row, first_row - 1, -1):
+            result.append(matrix[row][first_col])
+        first_col += 1
+
+    return result
+
+
+def matrix_in_spiral_order_pythonic(matrix: List[List[int]]) -> List[int]:
+    """
+    https://leetcode.com/problems/spiral-matrix/discuss/20571/1-liner-in-Python-%2B-Ruby
+
+    Test PASSED (51/51) [   1 ms]
+    Average running time:  390 us
+    Median running time:   162 us
+    """
+    return matrix and [*matrix.pop(0)] + matrix_in_spiral_order_pythonic([*zip(*matrix)][::-1])
+
+
+def square_matrix_in_spiral_order(square_matrix: List[List[int]]) -> List[int]:
+    """
+    Time complexity = O(n ^ 2) for an n x n square matrix.
+    Space complexity = O(1)
+
+    Test PASSED (51/51) [ 273 us]
+    Average running time:  103 us
+    Median running time:    75 us
+    """
+    size = len(square_matrix)
+    steps = size // 2
+    result = []
+    for step in range(steps):
+        for col in range(step, size - 1 - step):  # Top row from left to right.
+            result.append(square_matrix[step][col])
+        for row in range(step, size - 1 - step):  # Right column from top to bottom.
+            result.append(square_matrix[row][size - 1 - step])
+        for col in range(size - 1 - step, step, -1):  # Bottom row from right to left.
+            result.append(square_matrix[size - 1 - step][col])
+        for row in range(size - 1 - step, step, -1):  # Left column from bottom to top.
+            result.append(square_matrix[row][step])
+
+    if size % 2 == 1:  # Append the center element of the odd-sized matrix.
+        result.append(square_matrix[steps][steps])
+    return result
+
+
+def matrix_in_spiral_order_solution(square_matrix: List[List[int]]) -> List[int]:
+    """
     The time complexity is O(n ^ 2) where the size of the square matrix is n x n.
+
+    Test PASSED (51/51) [ 964 us]
+    Average running time:  266 us
+    Median running time:   143 us
     """
     def matrix_layer_in_clockwise(offset: int):
         # handle odd length case
