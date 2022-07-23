3224586fe96cf68329d835c69096331f717d07be
marcostar <sagarghadge24@gmail.com>
2022-07-24 00:26:17 +0530

    solved 5.2

--- epi_judge_python/int_as_array_increment.py
+++ epi_judge_python/int_as_array_increment.py
@@ -4,8 +4,31 @@ from test_framework import generic_test
 
 
 def plus_one(A: List[int]) -> List[int]:
-    # TODO - you fill in here.
-    return []
+    """
+    #5.2
+
+    Time complexity = O(n), where n is the length of A.
+    Space complexity = O(1)
+
+    Test PASSED (505/505) [   1 us]
+    Average running time:    2 us
+    Median running time:     2 us
+    """
+    if not A:
+        return A
+    A[-1] += 1
+    for i in range(len(A) - 1, 0, -1):
+        if A[i] != 10:
+            break
+        A[i] = 0
+        A[i - 1] += 1
+    if A[0] == 10:
+        # There is a carry-out, so we need one more digit to store the result.
+        # A slick way to do this is to append a 0 at the end of the array,
+        # and update the first entry to 1.
+        A[0] = 1
+        A.append(0)
+    return A
 
 
 if __name__ == '__main__':
