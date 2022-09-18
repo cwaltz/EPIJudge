95bd4e942b5610597e93a052108c994f7ea32da4
cwaltz <cwaltz@users.noreply.github.com>
2022-09-18 22:42:31 +0530

    solved 17.0

--- epi_judge_python/making_change.py
+++ epi_judge_python/making_change.py
@@ -2,8 +2,25 @@ from test_framework import generic_test
 
 
 def change_making(cents: int) -> int:
-    # TODO - you fill in here.
-    return 0
+    """
+    #17.0
+
+    Time complexity = O(1)
+                    = O(c), where c is the number of denominations of currency.
+    Space complexity = O(1)
+
+    Test PASSED (49999/49999) [   1 us]
+    Average running time:    1 us
+    Median running time:     1 us
+    """
+    coins = [100, 50, 25, 10, 5, 1]
+    num_coins = 0
+    for coin in coins:
+        num_coins += cents // coin
+        cents %= coin
+        if cents == 0:
+            break
+    return num_coins
 
 
 if __name__ == '__main__':
--- epi_judge_python_solutions/making_change.py
+++ epi_judge_python_solutions/making_change.py
@@ -2,7 +2,11 @@ from test_framework import generic_test
 
 
 def change_making(cents: int) -> int:
-
+    """
+    Test PASSED (49999/49999) [   1 us]
+    Average running time:    1 us
+    Median running time:     1 us
+    """
     coins = [100, 50, 25, 10, 5, 1]
     num_coins = 0
     for coin in coins:
