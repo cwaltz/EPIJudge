cab8901caca031a9fc59c56b95372a5a0803f941
cwaltz <cwaltz@users.noreply.github.com>
2022-10-13 15:47:10 +0530

    solved 8.3

--- epi_judge_python/is_valid_parenthesization.py
+++ epi_judge_python/is_valid_parenthesization.py
@@ -2,8 +2,30 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-    # TODO - you fill in here.
-    return True
+    """
+    #8.3
+
+    Time complexity = O(n), where n is the length of s
+    Space complexity = O(n) for stack
+
+    Test PASSED (78/78) [  <1 us]
+    Average running time:    8 us
+    Median running time:     1 us
+    """
+    if len(s) % 2 == 1:  # Odd length string can't be valid
+        return False
+    stack = []
+    closing = {
+        ')': '(',
+        '}': '{',
+        ']': '['
+    }
+    for c in s:
+        if c not in closing:  # c is an opening bracket so push it on stack
+            stack.append(c)
+        elif not stack or stack.pop() != closing[c]:
+            return False
+    return not stack
 
 
 if __name__ == '__main__':
--- epi_judge_python_solutions/is_valid_parenthesization.py
+++ epi_judge_python_solutions/is_valid_parenthesization.py
@@ -2,7 +2,11 @@ from test_framework import generic_test
 
 
 def is_well_formed(s: str) -> bool:
-
+    """
+    Test PASSED (78/78) [  <1 us]
+    Average running time:    8 us
+    Median running time:     1 us
+    """
     left_chars, lookup = [], {'(': ')', '{': '}', '[': ']'}
     for c in s:
         if c in lookup:
