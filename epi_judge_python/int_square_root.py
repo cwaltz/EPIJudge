b1b7a1d490c274732adcc0b442e0bf2343232b1a
cwaltz <cwaltz@users.noreply.github.com>
2022-10-15 22:23:15 +0530

    updated 4.7, 11.4

--- epi_judge_python/int_square_root.py
+++ epi_judge_python/int_square_root.py
@@ -10,25 +10,6 @@ def square_root(k: int) -> int:
     Time complexity = O(log k)
     Space complexity = O(1)
 
-    Test PASSED (2000/2000) [   5 us]
-    Average running time:    3 us
-    Median running time:     3 us
-    """
-    left, right = 0, k
-    # Candidate interval [left, right] where everything before left has square <= k,
-    # everything after right has square > k.
-    while left <= right:
-        mid = left + ((right - left) >> 1)
-        mid_squared = mid * mid
-        if mid_squared <= k:
-            left = mid + 1
-        else:  # k < mid_squared
-            right = mid - 1
-    return left - 1
-
-
-def square_root_simpler(k: int) -> int:
-    """
     Test PASSED (2000/2000) [   7 us]
     Average running time:    4 us
     Median running time:     4 us
@@ -47,6 +28,25 @@ def square_root_simpler(k: int) -> int:
     return result
 
 
+def square_root_faster(k: int) -> int:
+    """
+    Test PASSED (2000/2000) [   5 us]
+    Average running time:    3 us
+    Median running time:     3 us
+    """
+    left, right = 0, k
+    # Candidate interval [left, right] where everything before left has
+    # square <= k, everything after right has square > k.
+    while left <= right:
+        mid = left + ((right - left) >> 1)
+        mid_squared = mid * mid
+        if mid_squared <= k:
+            left = mid + 1
+        else:  # k < mid_squared
+            right = mid - 1
+    return left - 1
+
+
 def square_root_pythonic(k: int) -> int:
     """
     Test PASSED (2000/2000) [  <1 us]
@@ -54,6 +54,58 @@ def square_root_pythonic(k: int) -> int:
     Median running time:    <1 us
     """
     return math.floor(math.sqrt(k))
+
+
+def square_root_pocket_calculator_algorithm(k: int) -> int:
+    """
+    Time complexity = O(1)
+    Space complexity = O(1)
+
+    Test PASSED (2000/2000) [   1 us]
+    Average running time:    1 us
+    Median running time:     1 us
+    """
+    if k < 2:
+        return k
+    left = int(math.e ** (0.5 * math.log(k)))
+    right = left + 1
+    return left if right * right > k else right
+
+
+def square_root_recursion_and_bit_shifts(k: int) -> int:
+    """
+    Time complexity = O(log k)
+    Space complexity = O(log k)
+
+    Test PASSED (2000/2000) [   4 us]
+    Average running time:    2 us
+    Median running time:     2 us
+    """
+    if k < 2:
+        return k
+    left = square_root_recursion_and_bit_shifts(k >> 2) << 1
+    right = left + 1
+    return left if right * right > k else right
+
+
+def square_root_newtons_method(x: int) -> int:
+    """
+    Time complexity = O(log x)
+    Space complexity = O(1)
+
+    Test PASSED (2000/2000) [   3 us]
+    Average running time:    2 us
+    Median running time:     2 us
+    """
+    if x < 2:
+        return x
+    prev_root = x
+    root = (prev_root + x / prev_root) / 2
+    while abs(prev_root - root) >= 1:
+        prev_root = root
+        root = (prev_root + x / prev_root) / 2
+
+    return int(root)
 
 
 if __name__ == '__main__':
--- epi_judge_python/power_x_y.py
+++ epi_judge_python/power_x_y.py
@@ -1,27 +1,51 @@
+import math
+
 from test_framework import generic_test
 
 
 def power(x: float, y: int) -> float:
     """
+    #4.7
+
     Time complexity  = O(n), where n is number of bits needed to represent y.
     Space complexity = O(1)
 
-    The number of multiplications is at most twice the index of y's MSB, implying an O(n) time complexity.
+    The number of multiplications is at most twice the index of y's MSB,
+    implying an O(n) time complexity.
 
-    Test PASSED (10002/10002) [   3 us]
-    Average running time:    3 us
-    Median running time:     3 us
+    Test PASSED (10002/10002) [   2 us]
+    Average running time:    1 us
+    Median running time:     2 us
     """
     result, num = 1.0, y
     if y < 0:
         num = -num
         x = 1 / x
     while num:
-        if num & 1:
+        # if num & 1:
+        if num % 2:  # Might be faster than: num & 1
             result *= x
         num >>= 1
         x = x * x
     return result
+
+
+def power_direct(x: float, y: int) -> float:
+    """
+    Test PASSED (10002/10002) [  <1 us]
+    Average running time:   <1 us
+    Median running time:    <1 us
+    """
+    return x ** y
+
+
+def power_using_math_pow(x: float, y: int) -> float:
+    """
+    Test PASSED (10002/10002) [  <1 us]
+    Average running time:   <1 us
+    Median running time:    <1 us
+    """
+    return math.pow(x, y)
 
 
 if __name__ == '__main__':
