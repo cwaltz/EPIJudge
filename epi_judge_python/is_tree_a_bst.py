3191fc2bcbbbe48d18d47f8caad9b0cf51f4593a
cwaltz <cwaltz@users.noreply.github.com>
2022-11-21 01:09:18 +0530

    Update is_tree_a_bst.py

--- epi_judge_python/is_tree_a_bst.py
+++ epi_judge_python/is_tree_a_bst.py
@@ -43,21 +43,26 @@ def is_binary_tree_bst_iterative(tree: BinaryTreeNode) -> bool:
     """
     if not tree:
         return True
-    QueueEntry = collections.namedtuple('QueueEntry', ('node', 'lower', 'upper'))
-    bfs_queue = collections.deque([QueueEntry(tree, float('-inf'), float('inf'))])
+    QueueEntry = collections.namedtuple('QueueEntry', ('node', 'lower', 'upper')
+                                        )
+    bfs_queue = collections.deque([QueueEntry(tree, float('-inf'), float('inf'))
+                                   ])
 
     while bfs_queue:
         front = bfs_queue.popleft()
         if not front.lower <= front.node.data <= front.upper:
             return False
         if front.node.left:
-            bfs_queue.append(QueueEntry(front.node.left, front.lower, front.node.data))
+            bfs_queue.append(QueueEntry(front.node.left, front.lower,
+                                        front.node.data))
         if front.node.right:
-            bfs_queue.append(QueueEntry(front.node.right, front.node.data, front.upper))
+            bfs_queue.append(QueueEntry(front.node.right, front.node.data,
+                                        front.upper))
     return True
 
 
-def is_binary_tree_bst_iterative_without_namedtuple(tree: BinaryTreeNode) -> bool:
+def is_binary_tree_bst_iterative_without_namedtuple(tree: BinaryTreeNode)\
+        -> bool:
     """
     Time complexity = O(n), where n is the number of nodes
     Space complexity = O(n)
@@ -70,7 +75,8 @@ def is_binary_tree_bst_iterative_without_namedtuple(tree: BinaryTreeNode) -> boo
     """
     if not tree:
         return True
-    # QueueEntry = collections.namedtuple('QueueEntry', ('node', 'lower', 'upper'))
+    # QueueEntry = collections.namedtuple('QueueEntry', ('node', 'lower',
+    # 'upper'))
     bfs_queue = collections.deque([(tree, float('-inf'), float('inf'))])
 
     while bfs_queue:
