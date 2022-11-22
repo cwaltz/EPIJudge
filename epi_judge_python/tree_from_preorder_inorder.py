c6270c4d57f544fbd3e064d6c38686cf9f0a9dbc
cwaltz <cwaltz@users.noreply.github.com>
2022-11-22 18:33:13 +0530

    Update tree_from_preorder_inorder.py

--- epi_judge_python/tree_from_preorder_inorder.py
+++ epi_judge_python/tree_from_preorder_inorder.py
@@ -48,6 +48,53 @@ def binary_tree_from_preorder_inorder(preorder: List[int],
                                                     inorder_end=len(inorder))
 
 
+def binary_tree_from_preorder_inorder_shortest(
+        preorder: List[int], inorder: List[int]) -> Optional[BinaryTreeNode]:
+    """
+    Source: Neetcode :)
+    https://github.com/neetcode-gh/leetcode/blob/main/python/105-Construct-Binary-Tree-from-Preorder-and-Inorder-Traversal.py
+    # TODO: Watch Neetcode's video & calculate time & space complexities.
+
+    Test PASSED (3852/3852) [  <1 us]
+    Average running time:   65 us
+    Median running time:     6 us
+    """
+    if not preorder or not inorder:
+        return None
+
+    root = BinaryTreeNode(preorder[0])
+    mid = inorder.index(preorder[0])
+    root.left = binary_tree_from_preorder_inorder(
+        preorder[1: mid + 1], inorder[:mid])
+    root.right = binary_tree_from_preorder_inorder(
+        preorder[mid + 1:], inorder[mid + 1:])
+    return root
+
+
+def binary_tree_from_preorder_inorder_short(
+        preorder: List[int], inorder: List[int]) -> BinaryTreeNode:
+    """
+    Source:
+    https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/solutions/34543/simple-o-n-without-map/
+    # TODO: To be understood yet!
+
+    Test PASSED (3852/3852) [  <1 us]
+    Average running time:   49 us
+    Median running time:     5 us
+    """
+    def build(stop):
+        if inorder and inorder[-1] != stop:
+            root = BinaryTreeNode(preorder.pop())
+            root.left = build(root.data)
+            inorder.pop()
+            root.right = build(stop)
+            return root
+
+    preorder.reverse()
+    inorder.reverse()
+    return build(None)
+
+
 if __name__ == '__main__':
     exit(
         generic_test.generic_test_main('tree_from_preorder_inorder.py',
