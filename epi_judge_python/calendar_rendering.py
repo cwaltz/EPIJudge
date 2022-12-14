1b29fc9db73f30335f76f1ba3d09ce796bd3d6ef
cwaltz <cwaltz@users.noreply.github.com>
2022-12-14 18:33:16 +0530

    Update calendar_rendering.py

--- epi_judge_python/calendar_rendering.py
+++ epi_judge_python/calendar_rendering.py
@@ -12,8 +12,43 @@ Event = collections.namedtuple('Event', ('start', 'finish'))
 
 def find_max_simultaneous_events(events: List[Event]) -> int:
     """
-    13.6
+    #13.6
 
+    Time complexity = O(n log n), where n is the number of events.
+    Space complexity = O(n)
+
+    Source: Official leetcode solution :)
+    Approach 2: Chronological Ordering
+    https://leetcode.com/problems/meeting-rooms-ii/solutions/168762/meeting-rooms-ii/
+
+    Similar to Leetcode # 253
+
+    Only 11 lines of code! :)
+
+    Test PASSED (97/97) [  9 ms]
+    Average running time:  108 us
+    Median running time:     3 us
+    """
+    start_timings = sorted([i[0] for i in events])  # O(n log n)
+    end_timings = sorted(i[1] for i in events)  # O(n log n)
+    num_of_events = len(events)
+    start_index = end_index = used_rooms = 0
+    while start_index < num_of_events:  # O(n)
+        if start_timings[start_index] > end_timings[end_index]:
+            # One of the meetings ends by the start of current meeting so
+            # increment end_index & reuse that room for current meeting.
+            # No need to increment used_rooms.
+            end_index += 1
+        else:  # start_timings[start_index] < end_timings[end_index]
+            # All existing rooms are occupied so use a new room for current
+            # meeting & increment used_rooms.
+            used_rooms += 1
+        start_index += 1
+    return used_rooms
+
+
+def find_max_simultaneous_events_using_heap(events: List[Event]) -> int:
+    """
     Time complexity = O(n log n), where n is the number of events.
     Space complexity = O(n)
 
@@ -21,62 +56,27 @@ def find_max_simultaneous_events(events: List[Event]) -> int:
     Approach 1: Priority Queues
     https://leetcode.com/problems/meeting-rooms-ii/solutions/168762/meeting-rooms-ii/
 
-    Similar to Leetcode # 253
+    Only 8 lines of code! :)
 
-    Only 7 lines of code! :)
-
-    Test PASSED (97/97) [  11 ms]
-    Average running time:  126 us
+    Test PASSED (97/97) [  10 ms]
+    Average running time:  115 us
     Median running time:     3 us
     """
-    # The heap initialization
-    free_rooms = []
-
-    # Sort the events in increasing order of their start time.
-    events.sort(key=lambda x: x[0])
-
-    # For all the remaining meeting rooms
+    min_heap = []  # To store event end times
+    events.sort(key=lambda x: x[0])  # Non-decreasing order of event start times
     for event in events:
-
-        # If the room due to free up the earliest is free, assign that room
-        # to this meeting.
-        if free_rooms and free_rooms[0] < event[0]:
-            heapq.heappop(free_rooms)
-
-        # If a new room is to be assigned, then also we add to the heap.
-        # If an old room is allocated, then also we have to add to the heap
-        # with updated end time.
-        heapq.heappush(free_rooms, event[1])
-
-    # The size of the heap tells us the minimum rooms required for all the
-    # meetings.
-    return len(free_rooms)
-
-
-def find_max_simultaneous_events_neetcode(events: List[Event]) -> int:
-    """
-    Time complexity = O(n log n), where n is the number of events.
-    Space complexity = O(n)
-
-    Test PASSED (97/97) [  14 ms]
-    Average running time:  162 us
-    Median running time:     5 us
-    """
-    start_times = sorted([event.start for event in events])
-    finish_times = sorted([event.finish for event in events])
-
-    max_num_simultaneous_events = num_simultaneous_events = 0
-    start_index = finish_index = 0
-    while start_index < len(events):
-        if start_times[start_index] <= finish_times[finish_index]:
-            start_index += 1
-            num_simultaneous_events += 1
+        if min_heap and min_heap[0] < event[0]:
+            # An event room becomes free before the current event starts so
+            # assign that room to current event
+            heapq.heapreplace(min_heap, event[1])  # Pop & return the smallest
+            # item from the heap, & also push the new item. The heap size
+            # doesn't change. If the heap is empty, IndexError is raised.
         else:
-            finish_index += 1
-            num_simultaneous_events -= 1
-        if max_num_simultaneous_events < num_simultaneous_events:
-            max_num_simultaneous_events = num_simultaneous_events
-    return max_num_simultaneous_events
+            # Either min_heap is empty or all old rooms are occupied so assign a
+            # new room to current event
+            heapq.heappush(min_heap, event[1])
+    # Size of the heap tells us the minimum rooms required for all the events
+    return len(min_heap)
 
 
 def find_max_simultaneous_events_namedtuple(events: List[Event]) -> int:
