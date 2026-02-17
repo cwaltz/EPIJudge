7a0afb64d9f4adfff34391a65367b2a96b195234
cwaltz <cwaltz@users.noreply.github.com>
2026-02-17 23:56:03 +0530

    Update 4_primitive_types.py

--- epi_judge_python/4_primitive_types.py
+++ epi_judge_python/4_primitive_types.py
@@ -1,24 +1,24 @@
 import math
 
 
-print("6 & 4 = " + str(6 & 4))  # 4
-print("1 | 2 = " + str(1 | 2))  # 3
-print("8 >> 1 = " + str(8 >> 1))  # 4
-print("-16 >> 2 = " + str(-16 >> 2))  # -4
-print("1 << 10 = " + str(1 << 10))  # 1024
-print("~0 = " + str(~0))  # -1
-print("~1 = " + str(~1))  # -2
-print("15 ^ 10 = " + str(15 ^ 10))  # 5
-print("abs(-34.5) =", abs(-34.5))  # 34.5
+print(f"6 & 4 = {6 & 4}")  # 4
+print(f"1 | 2 = {1 | 2}")  # 3
+print(f"8 >> 1 = {8 >> 1}")  # 4
+print(f"-16 >> 2 = {-16 >> 2}")  # -4
+print(f"1 << 10 = {1 << 10}")  # 1024
+print(f"~0 = {~0}")  # -1
+print(f"~1 = {~1}")  # -2
+print(f"15 ^ 10 = {15 ^ 10}")  # 5
+print(f"abs(-34.5) = {abs(-34.5)}")  # 34.5
 
-print("math.ceil(2.17) =", math.ceil(2.17))  # 3
-print("math.ceil(-2.17) =", math.ceil(-2.17))  # -2
-print("math.floor(3.14) =", math.floor(3.14))  # 3
-print("math.floor(-3.14) =", math.floor(-3.14))  # -4
-print("pow(2.71, 3.14) =", pow(2.71, 3.14))  # 22.883559193263366
-print("pow(2, 3) =", pow(2, 3))  # 8
-print("3 ** 2 =", 3 ** 2)  # 9
-print("math.sqrt(225) =", math.sqrt(225))  # 15.0
+print(f"math.ceil(2.17) = {math.ceil(2.17)}")  # 3
+print(f"math.ceil(-2.17) = {math.ceil(-2.17)}")  # -2
+print(f"math.floor(3.14) = {math.floor(3.14)}")  # 3
+print(f"math.floor(-3.14) = {math.floor(-3.14)}")  # -4
+print(f"pow(2.71, 3.14) = {pow(2.71, 3.14)}")  # 22.883559193263366
+print(f"pow(2, 3) = {pow(2, 3)}")  # 8
+print(f"3 ** 2 = {3 ** 2}")  # 9
+print(f"math.sqrt(225) = {math.sqrt(225)}")  # 15.0
 
 
 def right_propagate_rightmost_set_bit(x: int) -> int:
@@ -39,7 +39,8 @@ def compute_x_modulo_a_power_of_two(x: int) -> int:
     time.
     Compute x modulo a power of two, e.g., returns 13 for 77 mod 64.
     """
-    pass
+    highest_power_of_2 = 1 << (x.bit_length() - 1)
+    return x ^ highest_power_of_2
 
 
 def test_if_x_is_a_power_of_2(x: int) -> bool:
