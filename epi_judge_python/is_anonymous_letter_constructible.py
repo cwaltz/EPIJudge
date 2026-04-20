22cae4f8181670a33a3a733a937fb05919d56f3f
cwaltz <12824565+cwaltz@users.noreply.github.com>
2026-04-20 08:57:28 +0530

    Update 12.2

--- epi_judge_python/is_anonymous_letter_constructible.py
+++ epi_judge_python/is_anonymous_letter_constructible.py
@@ -1,4 +1,4 @@
-import collections
+from collections import Counter
 from test_framework import generic_test
 
 
@@ -7,18 +7,28 @@ def is_letter_constructible_from_magazine(letter_text: str,
     """
     #12.2
 
-    Time complexity  = O(m + n), where m and n are the number of characters in the letter and magazine, respectively.
-    Space complexity = O(L), where L is the number of distinct characters appearing in the letter.
-                     = the size of the hash table constructed in the pass over the letter.
+    Time complexity  = O(m + n), where m and n are the number of characters in
+        the letter and magazine, respectively.
+    Space complexity = O(L), where L is the number of distinct characters
+        appearing in the letter.
+                     = the size of the hash table constructed in the pass over
+                     the letter.
 
-    Test PASSED (212/212) [  15 ms]
-    Average running time:  107 us
-    Median running time:    19 us
+    Test PASSED (212/212) [  13 ms]
+    Average running time:   90 us
+    Median running time:    15 us
     """
-    # Compute the frequencies for all chars in letter_text.
-    char_frequency_for_letter = collections.Counter(letter_text)
+    if not letter_text:
+        return True
 
-    # Checks if characters in magazine_text can cover characters in char_frequency_for_letter.
+    if not magazine_text:
+        return False
+
+    # Compute the frequencies for all chars in letter_text.
+    char_frequency_for_letter = Counter(letter_text)
+
+    # Checks if characters in magazine_text can cover characters in
+    # char_frequency_for_letter.
     for c in magazine_text:
         if c in char_frequency_for_letter:
             char_frequency_for_letter[c] -= 1
@@ -28,9 +38,7 @@ def is_letter_constructible_from_magazine(letter_text: str,
                     # All characters for letter_text are matched.
                     return True
 
-    # Empty char_frequency_for_letter means every char in letter_text can be
-    # covered by a character in magazine_text.
-    return not char_frequency_for_letter
+    return False
 
 
 # Pythonic solution that exploits collections.Counter.
@@ -38,12 +46,11 @@ def is_letter_constructible_from_magazine(letter_text: str,
 def is_letter_constructible_from_magazine_pythonic(letter_text: str,
                                                    magazine_text: str) -> bool:
     """
-    Test PASSED (212/212) [  26 ms]
-    Average running time:  151 us
-    Median running time:    16 us
+    Test PASSED (212/212) [  23 ms]
+    Average running time:  134 us
+    Median running time:    14 us
     """
-    return (not collections.Counter(letter_text) -
-            collections.Counter(magazine_text))
+    return not Counter(letter_text) - Counter(magazine_text)
 
 
 if __name__ == '__main__':
