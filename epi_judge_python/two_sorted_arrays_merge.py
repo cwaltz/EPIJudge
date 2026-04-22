48b33de5c3e7072d8f57c2ec4c3a4bf1598c406f
cwaltz <12824565+cwaltz@users.noreply.github.com>
2026-04-22 10:14:55 +0530

    Update 13.2

--- epi_judge_python/two_sorted_arrays_merge.py
+++ epi_judge_python/two_sorted_arrays_merge.py
@@ -1,36 +1,36 @@
-from typing import List
-
 from test_framework import generic_test
 
 
-def merge_two_sorted_arrays(A: List[int], m: int, B: List[int],
+def merge_two_sorted_arrays(first: list[int], m: int, second: list[int],
                             n: int) -> None:
     """
     #13.2
 
-    Time complexity = O(m + n), where m and n are the number of entries initially in the first and second arrays.
+    Time complexity = O(m + n), where m and n are the number of entries
+        initially in the first and second arrays.
     Space complexity = O(1)
 
-    Test PASSED (201/201) [   6 ms]
-    Average running time:   40 us
-    Median running time:     4 us
+    Test PASSED (201/201) [   5 ms]
+    Average running time:   32 us
+    Median running time:     2 us
     """
     i, j, write_idx = m - 1, n - 1, m + n - 1
     while i >= 0 and j >= 0:
-        if A[i] < B[j]:
-            A[write_idx] = B[j]
+        if first[i] < second[j]:
+            first[write_idx] = second[j]
             j -= 1
-        else:  # B[j] <= A[i]
-            A[write_idx] = A[i]
+        else:  # second[j] <= first[i]
+            first[write_idx] = first[i]
             i -= 1
         write_idx -= 1
     if j >= 0:
-        A[:j + 1] = B[:j + 1]
+        first[:j + 1] = second[:j + 1]
 
 
-def merge_two_sorted_arrays_wrapper(A, m, B, n):
-    merge_two_sorted_arrays(A, m, B, n)
-    return A
+def merge_two_sorted_arrays_wrapper(first: list[int], m: int, second: list[int],
+                                    n: int) -> list[int]:
+    merge_two_sorted_arrays(first, m, second, n)
+    return first
 
 
 if __name__ == '__main__':
