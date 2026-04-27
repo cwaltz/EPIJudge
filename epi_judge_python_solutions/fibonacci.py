27972df902522b40129c077a2283ab9a5fc1139e
cwaltz <12824565+cwaltz@users.noreply.github.com>
2026-04-27 08:01:50 +0530

    Update 16.0

--- epi_judge_python_solutions/fibonacci.py
+++ epi_judge_python_solutions/fibonacci.py
@@ -3,7 +3,9 @@ import functools
 from test_framework import generic_test
 
 
-@functools.lru_cache(None)  # maxsize is set to None, the LRU feature is disabled & the cache can grow without bound.
+@functools.lru_cache(None)
+# maxsize is set to None, the LRU feature is disabled & the cache can grow
+# without bound.
 def fibonacci(n: int) -> int:
     """
     Test PASSED (46/46) [  <1 us]
