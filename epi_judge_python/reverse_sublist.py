5205bd6e29885aef0bbe75bca000b41e085ea2dd
cwaltz <12824565+cwaltz@users.noreply.github.com>
2026-05-16 19:06:58 +0530

    Update 7.2

--- epi_judge_python/reverse_sublist.py
+++ epi_judge_python/reverse_sublist.py
@@ -1,27 +1,26 @@
-from typing import Optional
-
 from list_node import ListNode
 from test_framework import generic_test
 
 
-def reverse_sublist(L: ListNode, start: int,
-                    finish: int) -> Optional[ListNode]:
+def reverse_sublist(node: ListNode, start: int,
+                    finish: int) -> ListNode | None:
     """
     #7.2
 
-    Time complexity = O(f), the time complexity is dominated by the search for the fth node.
+    Time complexity = O(f), the time complexity is dominated by the search for
+        the fth node.
     Space complexity = O(1)
 
-    Test PASSED (210/210) [   2 ms]
-    Average running time:   17 us
-    Median running time:     2 us
+    Test PASSED (210/210) [   1 ms]
+    Average running time:    9 us
+    Median running time:     1 us
     """
-    dummy_head = sublist_head = ListNode(0, L)
+    dummy_head = sublist_head = ListNode(0, node)
     for _ in range(1, start):
-        sublist_head = sublist_head.next
+        sublist_head = sublist_head.next  # Dummy head of the sublist
 
     # Reverses sublist.
-    sublist_iter = sublist_head.next
+    sublist_iter = sublist_head.next  # Old head as well as new tail of sublist
     for _ in range(finish - start):
         temp = sublist_iter.next
         sublist_iter.next, temp.next, sublist_head.next = (temp.next,
@@ -32,5 +31,6 @@ def reverse_sublist(L: ListNode, start: int,
 
 if __name__ == '__main__':
     exit(
-        generic_test.generic_test_main('reverse_sublist.py',
-                                       'reverse_sublist.tsv', reverse_sublist))
+        generic_test.generic_test_main(
+            'reverse_sublist.py',
+            'reverse_sublist.tsv', reverse_sublist))
