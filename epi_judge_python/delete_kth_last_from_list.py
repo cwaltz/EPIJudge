1f0b3be1b0e9fe1ed581f7a42cafe9d486b60e9f
cwaltz <12824565+cwaltz@users.noreply.github.com>
2026-06-14 12:13:36 +0530

    Update 7.7

--- epi_judge_python/delete_kth_last_from_list.py
+++ epi_judge_python/delete_kth_last_from_list.py
@@ -1,27 +1,27 @@
-from typing import Optional
-
 from list_node import ListNode
 from test_framework import generic_test
 
 
 # Assumes L has at least k nodes, deletes the k-th last node in L.
-def remove_kth_last(L: ListNode, k: int) -> Optional[ListNode]:
+def remove_kth_last(head: ListNode, k: int) -> ListNode | None:
     """
     #7.7
 
     Time complexity = O(n), where n is the length of the list.
     Space complexity = O(1)
 
-    Test PASSED (306/306) [   2 ms]
-    Average running time:   10 us
-    Median running time:     2 us
+    Test PASSED (306/306) [   1 ms]
+    Average running time:    7 us
+    Median running time:     1 us
     """
-    dummy_head = pre = advanced = ListNode(0, L)
+    dummy_head = pre = advanced = ListNode(0, head)
     for _ in range(k + 1):
         advanced = advanced.next
+
     while advanced:
         advanced = advanced.next
         pre = pre.next
+
     # predecessor's next node is kth last.
     pre.next = pre.next.next
     return dummy_head.next
